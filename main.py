10752a14af08a06bc42871490d2a348f7641694e
Gabriel Nuernberg Biazoto <biazotogabriel@gmail.com>
2023-03-03 16:54:34 -0300

    Criado função de exibição de mensagem

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ def gether_data():
     n2 = int(input('Segundo valor: '))
     return n1, n2
 
+def print_message(n1, n2):
+    print(f'Os valores {n1} e {n2} somados dão {n1+n2}')
+
+    return None
+
 def main():
     n1, n2 = gether_data()
     print(n1**n2)
